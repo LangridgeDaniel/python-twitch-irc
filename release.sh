1f271fb371cbd311636ac369e2ca9b5f375fb60a
jpaulsen <jpaulsen@lo3energy.com>
2018-08-11 19:04:55 -0400

    README.me required for build in container, release.sh
    Signed-off-by: jpaulsen <jpaulsen@lo3energy.com>

--- release.sh
+++ release.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 set -ex
 
-export TWINE_REPOSITORY_URL=https://pypi.org/legacy/
-
-# twine upload --repository-url https://test.pypi.org/legacy/ dist/*
+# export TWINE_REPOSITORY_URL=https://pypi.org/
 
 docker build -t twitch-irc-release \
   --target release .
@@ -12,5 +10,4 @@ docker run --rm \
   -v ${PWD}/dist:/dist \
   -e TWINE_USERNAME \
   -e TWINE_PASSWORD \
-  -e TWINE_REPOSITORY_URL \
   twitch-irc-release /dist/*
